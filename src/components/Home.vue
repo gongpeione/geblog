f0070067a8336765891dc46f3afe5bc9964d4228
Geeku <ggongpei@qq.com>
2017-02-08 17:29:58 +0800

    blog page init

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrap">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
