8de47ae40cad98ba4fad633f7f94efce648e62b6
gongpeione <pei.gong@ele.me>
2018-04-13 19:28:23 +0800

    Middleware

--- main.go
+++ main.go
@@ -1,30 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
-
-type Person struct {
-	Name    string `form:"name"`
-	Address string `form:"address"`
-}
 
 var router *gin.Engine
 
 func main() {
 	router = gin.Default()
-	initializeRoutes()
-	// route.Any("/", func(c *gin.Context) {
-	// 	var person Person
-	// 	if c.ShouldBindQuery(&person) == nil {
-	// 		log.Println("====== Only Bind By Query String ======")
-	// 		log.Println(person.Name)
-	// 		log.Println(person.Address)
-	// 	}
-	// 	c.String(200, "Success")
-	// })
+	routerInit()
 	router.Run(":8085")
-	fmt.Printf("hello, world\n")
 }
--- router.go
+++ router.go
@@ -1,18 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
 
-func initializeRoutes() {
-	// router.Static("uploads/", "./public/uploads")
+	"github.com/gin-gonic/gin"
+)
+
+func routerInit() {
 
 	admin := router.Group("/admin")
+	admin.Use()
 	{
 		admin.GET("/login", func(c *gin.Context) {
 			c.String(200, "Login")
 		})
 	}
 
+	api := router.Group("/api")
+	{
+		admin := api.Group("/admin")
+		admin.Use(AuthRequired())
+		{
+			admin.GET("/login", func(c *gin.Context) {
+				c.String(200, "login")
+			})
+		}
+
+		api.GET("/", func(c *gin.Context) {
+
+		})
+	}
+
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "package main")
 	})
+}
+
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Println("MMMMMMMMMMMMMM")
+		c.Next()
+	}
 }
